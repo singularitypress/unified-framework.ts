b0d96a4f688c44accadb6ec376c86efdf5832941
singularitypress <jaypandya2k5@gmail.com>
2020-10-06 14:33:29 -0400

    eslint, tsconfig, vscode format rules/ enforcement

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,33 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
-    'standard'
+    "standard",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
   plugins: [
-    '@typescript-eslint'
+    "@typescript-eslint",
   ],
   rules: {
-  }
-}
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "never",
+        exports: "always-multiline",
+        functions: "always-multiline",
+      },
+    ],
+    "no-undef": 0,
+    "no-unused-vars": 0,
+  },
+};
