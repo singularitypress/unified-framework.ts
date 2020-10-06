70436f36e6cf3ef4fb3c327b026c363dfbc1b1ba
singularitypress <jaypandya2k5@gmail.com>
2020-10-06 16:32:43 -0400

    added express, basic types

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,20 +1,32 @@
-import { graphql, buildSchema } from "graphql";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
-// Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
-// The root provides a resolver function for each API endpoint
 const root = {
   hello: () => {
-    return "Hello world!";
+    return "hello world";
   },
+  quoteOfTheDay: () => Math.random() < 0.5 ? "Take it easy" : "Salvation lies within",
+  random: () => Math.random(),
+  rollThreeDice: () => [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6)),
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, "{ hello }", root).then((response) => {
-  console.log(response);
-});
+const app = express();
+
+app.use("/graphql", graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
