9643e03ba1e448f0fd71a1173049a28fb18dae4f
singularitypress <jaypandya2k5@gmail.com>
2020-10-16 18:16:21 -0400

    s3e11 todo: create fake users db

--- typescript/index.ts
+++ typescript/index.ts
@@ -13,19 +13,6 @@ const schema = buildSchema(`
 `);
 
 const root = {
-  hello: () => {
-    return "hello world";
-  },
-  quoteOfTheDay: () => Math.random() < 0.5 ? "Take it easy" : "Salvation lies within",
-  random: () => Math.random(),
-  rollThreeDice: () => [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6)),
-  rollDice: ({ numDice, numSides }: {numDice: number; numSides: number}) => {
-    const output = [] as number[];
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
-  },
 };
 
 const app = express();
