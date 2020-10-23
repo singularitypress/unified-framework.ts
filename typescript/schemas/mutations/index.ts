4a5a5e7f71bde519758b290ad058ad6ba518ede1
singularitypress <jaypandya2k5@gmail.com>
2020-10-23 12:36:49 -0400

    Completed CRUD basic operations

--- typescript/schemas/mutations/index.ts
+++ typescript/schemas/mutations/index.ts
@@ -26,5 +26,36 @@ export const mutation = new GraphQLObjectType({
         }).then(res => res.data);
       },
     },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve (parentValue, args) {
+        Axios.delete(`http://localhost:3000/users/${args.id}`).then(res => res.data);
+      },
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        firstName: {
+          type: GraphQLString,
+        },
+        age: {
+          type: GraphQLInt,
+        },
+        companyId: {
+          type: GraphQLString,
+        },
+      },
+      resolve (parentValue, args) {
+        return Axios.patch(`http://localhost:3000/users/${args.id}`, args).then(res => res.data);
+      },
+    },
   },
 });
