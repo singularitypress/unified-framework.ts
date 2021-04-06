a4bd3a24a92ab088bc37e7ca36534b8b3f39af60
jaypandya2k5+laptop <jaypandya2k5+laptop@gmail.com>
2021-04-06 15:14:49 -0400

    remove unused route

--- server/index.ts
+++ server/index.ts
@@ -2,16 +2,12 @@ import express from "express";
 import compression from "compression";
 import { graphqlHTTP } from "express-graphql";
 import { schema } from "./schema";
-
-import fs from "fs";
-import path from "path";
-import { bnsParser } from "./services";
-
 require("dotenv").config();
 
 const app = express();
 
 app.use(compression());
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -19,14 +15,6 @@ app.use(
     graphiql: true,
   }),
 );
-
-app.get("/xlsx", (req, res) => {
-  let arr = [] as any[];
-  if (process.env.BNS) {
-    arr = [...bnsParser(path.resolve(process.env.BNS))];
-  }
-  res.send(arr);
-});
 
 app.listen(4000);
 console.log("Running a GraphQL API server at http://localhost:4000/graphql");
